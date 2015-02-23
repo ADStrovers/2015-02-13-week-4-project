c40de9e8f8c0cc6754cde98fe3f050ddff906a9c
Andrew Strovers <ADStrovers@gmail.com>
2015-02-23 10:33:44 -0600

    Minot changes.

--- helpers/get_map.rb
+++ helpers/get_map.rb
@@ -15,4 +15,5 @@ module GetMap
   def get_map(obj)
     map = "<img src=\"https://maps.googleapis.com/maps/api/staticmap?size=360x360&zoom=15&center=#{obj.address}&markers=#{obj.address}\">"
   end
+  
 end
